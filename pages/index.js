48ac8940eb93c186697219375035e02fbe5007d0
Enéas da Silva Junior <35938994+EneasFeynlinus@users.noreply.github.com>
2026-06-20 23:45:38 +0000

    atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Make plans like an adult an believ in them like a child! Its my life its now or never</h1>
+    return <h1>Make plans like an adult an believ in them like a child! Its my life its now or never. You have power over your mind, not outside events.</h1>
 }
 
 export default Home;
