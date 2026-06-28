bf723314dfba9a4efe3b9431caa6b993da35dac7
Enéas da Silva Junior <35938994+EneasFeynlinus@users.noreply.github.com>
2026-06-28 19:22:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Make plans like an adult an believ in them like a child! Its my life its now or never. You have power over your mind, not outside events.</h1>
+function Home() {
+  return (
+    <h1>
+      Make plans like an adult an believ in them like a child! Its my life its
+      now or never. You have power over your mind, not outside events.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
