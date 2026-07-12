8bea1fdd0e5d257f933dae70cf19deddef4237b5
Enéas da Silva Junior <35938994+EneasFeynlinus@users.noreply.github.com>
2026-07-12 21:44:04 +0000

    Atualizando goals

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,14 @@ function Home() {
   return (
     <h1>
       Make plans like an adult an believ in them like a child! Its my life its
-      now or never. You have power over your mind, not outside events.
+      now or never. You have power over your mind, not outside events. 1) Feito
+      - Uma aba superior, com 2 abas superiores com formato de colunas gregas,
+      no centro um local de meditação com fontes. Local para clicar escolhendo
+      sons, velocidade e tipos de respiração. Uma tabula que será disponivel
+      quando inativo, para a comunidade deixar dicas e comentários. 2) Dados dos
+      usuários 3) Tentar todo dia melhorar o site e pegar esse hábito
     </h1>
   );
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
 }
 
 export default Home;
